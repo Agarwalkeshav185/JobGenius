604519ef6e42d90367ab46945c02c411cabba7f8
agarwalkeshav185 <agarwalkeshav185@gmail.com>
2025-04-07 12:57:22 +0530

    Setup of Database COmpleted

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import ApiRoutes from './Routes/index.js';
+import cors from 'cors';
+import { FRONTEND_URL } from './config/serverConfig.js';
 
-const app = 
+const app = express();
+
+app.use(cors({
+    origin : [FRONTEND_URL],
+    methods : ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials : true
+}));
+
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use('/api', ApiRoutes);
+
+
+export default app;
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
 import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME, PORT } from './config/serverConfig.js';
 import cloudinary from 'cloudinary';
 import app from './app.js';
+import { connect } from './config/database.js';
 
 cloudinary.v2.config({
     cloud_name : CLOUDINARY_CLOUD_NAME,
@@ -9,6 +9,8 @@ cloudinary.v2.config({
     api_secret : CLOUDINARY_API_SECRET
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, async ()=>{
     console.log(`Server Started at port :- ${PORT}`);
-})
+    await connect();
+    console.log('MongoDB Connected');
+});
